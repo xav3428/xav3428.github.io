16870bb3e5e50236f0be38bff1265e243a7ed642
Xavier Bouchard <2039817@cegepat.qc.ca>
2023-03-23 10:33:31 -0400

    Ajout de prettier pour formater le tout

--- fadeinout.js
+++ fadeinout.js
@@ -1,14 +1,16 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
-const elementsToObserve = document.querySelectorAll('.hiddenLeft, .hiddenRight');
+const elementsToObserve = document.querySelectorAll(
+  ".hiddenLeft, .hiddenRight"
+);
 elementsToObserve.forEach((el) => {
-    observer.observe(el);
+  observer.observe(el);
 });
